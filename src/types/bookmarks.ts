ab5c1697a441fa89038ae7e73d536e8aaa24ad9f
hichem <hichem.toumi.2001@gmail.com>
2025-08-09 22:58:18 +0100

    types for bookmarks

--- src/types/bookmarks.ts
+++ src/types/bookmarks.ts
@@ -1,9 +1,5 @@
-// export type BookmarkType = {
-//   chapter_number: number;
-//   verse_number: number;
-// };
-
 import { WQuranVerse } from "./w-quran";
+import { WResult } from "./w-result";
 
 export type BookmarkType = Omit<
   WQuranVerse,
@@ -18,4 +14,25 @@ export type BookmarkType = Omit<
   | "verse_subtitle_english"
   | "verse_footnote_english"
   | "word_by_word"
->;
+> & {
+  bookmark_datetime_timezoneaware: string;
+  notes: string;
+};
+
+export type BookmarkInjectedType = BookmarkType & WQuranVerse;
+
+export interface WBookmarkQuranAPIResponse {
+  message: string;
+  request: {
+    type: WResult["request"]["type"];
+    parsed_query: any;
+    raw_query: string;
+  };
+  response: {
+    data: BookmarkInjectedType[];
+    copyright?: {
+      text: string;
+      url: string;
+    };
+  };
+}
